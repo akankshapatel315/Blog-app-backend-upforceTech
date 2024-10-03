dbc9d32cdf7d78efa6982ba845854ad2ba4cc141
cuneiform-akankshapatel <akanksha.patel@thecuneiform.com>
2024-10-04 03:05:42 +0530

    url added

--- src/config/dbConnection.ts
+++ src/config/dbConnection.ts
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 function connectDB() {
-  const url = "mongodb://localhost:27017/BlogApp";
-
+  // const url = "mongodb://localhost:27017/BlogApp";
+const url="mongodb+srv://akankshapatel315:Ak%40nkshaisthebest@cluster0.8am0v.mongodb.net/blog-app"
   try {
     mongoose.connect(url, {
       useNewUrlParser: true,
