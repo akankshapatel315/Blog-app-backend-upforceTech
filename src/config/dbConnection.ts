03da4abec28a2e64d4c1f5e0327b0abe6b2b94ef
cuneiform-akankshapatel <akanksha.patel@thecuneiform.com>
2024-10-04 03:19:14 +0530

    mongo conn changes

--- src/config/dbConnection.ts
+++ src/config/dbConnection.ts
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 
 function connectDB() {
   // const url = "mongodb://localhost:27017/BlogApp";
-const url="mongodb+srv://akankshapatel315:Ak%40nkshaisthebest@cluster0.8am0v.mongodb.net/blog-app"
+  const url = "mongodb+srv://akankshapatel315:Ak%40nkshaisthebest@cluster0.8am0v.mongodb.net/blog-app"
   try {
     mongoose.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).then((res:any)=>
-    {
+      useNewUrlParser: true, 
+      useUnifiedTopology: true ,
+      serverSelectionTimeoutMS: 20000, 
+      socketTimeoutMS: 45000 
+    }).then((res: any) => {
       console.log("connected MongoDB")
     });
   } catch (err: any) {
